251d15595e50888e80b045772fe8853e29ce9eb1
maroqijalil <jalilpro31@gmail.com>
2021-06-18 20:12:12 +0700

    feat: add dump

--- client/program_client_dump.c
+++ client/program_client_dump.c
@@ -7,11 +7,12 @@
 #include <string.h>
 #include <unistd.h>
 
-void main(int argc, char *argv[]) {
-  if (argc < 8) {
+int main(int argc, char *argv[])
+{
+  if (argc < 6) {
     printf("Masukkan argumen yang benar untuk melakukan dump database\n");
-    printf("Format: ./[program_dump_database] -u [username] -p [password] \
-      [nama_database] > [nama_file_backup]\n");
+    printf("Format: ./[program_dump_database] -u [username] -p [password] [nama_database] > [nama_file_backup]\n");
+    return EXIT_FAILURE;
   }
 
   char username[NAME_MAX];
@@ -24,15 +25,13 @@ void main(int argc, char *argv[]) {
     } else if (!strcmp(argv[i], "-p")) {
       strcpy(password, argv[i + 1]);
       strcpy(database, argv[i + 2]);
-    } else if (!strcmp(argv[i], ">")) {
-      strcpy(filename, argv[i + 1]);
     }
   }
 
   char current_dir[NAME_MAX];
   if (getcwd(current_dir, sizeof(current_dir)) == NULL) {
     perror("Tidak dapat mendapatkan path sekarang ini!");
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
   char database_dir[PATH_MAX];
@@ -42,21 +41,7 @@ void main(int argc, char *argv[]) {
   openned_dir = opendir(database_dir);
   if (openned_dir == NULL) {
     perror("Database tidak ditemukan!");
-    exit(EXIT_FAILURE);
-  }
-
-  char backup_file_dir[PATH_MAX];
-  sprintf(backup_file_dir, "%s/%s", current_dir, filename);
-  if (!access(backup_file_dir, F_OK)) {
-    perror("File backup sudah ada!");
-    exit(EXIT_FAILURE);
-  }
-
-  FILE * backup_file;
-  backup_file = fopen(backup_file_dir, "a");
-  if (backup_file == NULL) {
-    perror("Tidak dapat membuat file backup!");
-    exit(EXIT_FAILURE);
+    return EXIT_FAILURE;
   }
 
   struct dirent *item;
@@ -66,23 +51,117 @@ void main(int argc, char *argv[]) {
     }
 
     char item_dir[PATH_MAX];
-    sprintf(item_dir, "%s/%s", current_dir, item->d_name);
+    sprintf(item_dir, "%s/%s", database_dir, item->d_name);
 
-    FILE * item_file;
+    FILE *item_file;
     item_file = fopen(item_dir, "r");
     if (item_file == NULL) {
       perror("Tidak dapat membuka file!");
       continue;
     }
 
-    fprintf(backup_file, "DROP TABLE %s;\n", item->d_name);
+    printf("DROP TABLE %s;\n", item->d_name);
 
-    char * text = NULL;
-    size_t length = 0;
+    char *txt = NULL;
+    size_t len = 0;
+    ssize_t txt_len;
     bool is_column_name = true;
-    // kurang membaca file per table nya
+    
+    int col_counter = 0;
+    bool is_col_num[50];
+    while ((txt_len = getline(&txt, &len, item_file)) != -1) {
+      char query[PATH_MAX];
+
+      if (is_column_name) {
+        sprintf(query, "CREATE TABLE %s (", item->d_name);
+
+        char buff[NAME_MAX];
+        strcpy(buff, txt);
+
+        char column[50];
+        char *substr = strtok(buff, ";");
+        while(substr != NULL) {
+          for (int i = 0; i < strlen(substr); i++) {
+            if (i == strlen(substr) - 1) {
+              column[i] = '\0';
+              strcat(query, column);
+
+              if (substr[i] == 'i') {
+                strcat(query, " int");
+                is_col_num[col_counter] = true;
+              } else if (substr[i] == 's') {
+                strcat(query, " string");
+                is_col_num[col_counter] = false;
+              }
+
+              col_counter++;
+            } else {
+              column[i] = substr[i];
+            }
+          }
+
+          substr = strtok(NULL, ";");
+          if (substr != NULL && strlen(substr)) {
+            strcat(query, ", ");
+          } else {
+            col_counter--;
+            query[strlen(query) - strlen(", ")] = '\0';
+            strcat(query, ");\n");
+          }
+        }
+
+        is_column_name = false;
+      } else {
+        sprintf(query, "INSERT INTO %s (", item->d_name);
+
+        char buff[NAME_MAX];
+        strcpy(buff, txt);
+
+        int val_counter = 0;
+        int char_counter = 0;
+        char val[NAME_MAX];
+        for (int i = 0; i < strlen(buff); i++) {
+          if (buff[i] != ';') {
+            val[char_counter] = buff[i];
+          } else if (buff[i] == ';') {
+            if (buff[i - 1] == '\\') {
+              val[char_counter] = buff[i];
+            } else {
+              char data[NAME_MAX];
+              strcpy(data, val);
+              data[char_counter] = '\0';
+
+              if (is_col_num[val_counter]) {
+                strcat(query, data);
+              } else {
+                strcat(query, "'");
+                strcat(query, data);
+                strcat(query, "'");
+              }
+
+              val_counter++;
+              if (val_counter == col_counter) {
+                strcat(query, ");");
+              } else {
+                strcat(query, ", ");
+                char_counter = -1;
+              }
+            }
+          }
+
+          char_counter++;
+        }
+      }
+
+      printf("%s\n", query);
+    }
+    printf("\n");
+
+    fclose(item_file);
+    if (txt) {
+      free(txt);
+    }
   }
 
-  fclose(backup_file);
-  exit(EXIT_SUCCESS);
+  return EXIT_SUCCESS;
 }
